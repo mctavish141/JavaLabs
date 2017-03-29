1a45644bab7d8aea089ed2ddea3beae384a9d6e6
mctavish141 <serega.kiev.ua@gmail.com>
2017-03-29 15:50:35 +0300

    Create PlaneResource.java

---
+++ FlightsProject/src/com/flights/Resources/PlaneResource.java
@@ -0,0 +1,41 @@
+package com.flights;
+
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/planes")
+public static class PlaneResource {
+  @GET
+  @Produces(MediaType.APPLICATION_XML)
+  public List<Plane> getAllPlanes() {
+    return PlaneAccess.readAll();
+  }
+  
+  @POST
+  @Consumes(MediaType.APPLICATION_XML)
+  public void createPlane(String manufacturer, String model, int seats, Airline airline, int amount) {
+    PlaneAccess.create(manufacturer, model, seats, airline, amount);
+  }
+  
+  @PUT
+  @Consumes(MediaType.APPLICATION_XML)
+  public void updatePlane(int id, String manufacturer, String model, int seats, Airline airline, int amount) {
+    PlaneAccess.update(id, manufacturer, model, seats, airline, amount);
+  }
+  
+  @DELETE
+  @Path("/{id}")
+  @Consumes(MediaType.APPLICATION_XML)
+  public void deletePlane(@PathParam("id") int id) {
+    PlaneAccess.delete(id);
+  }
+}
